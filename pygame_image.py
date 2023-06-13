a94c58750b8a31e905612ea02cafd990e92e2432
ryuseiy <c0b2215016@edu.teu.ac.jp>
2023-06-13 16:29:22 +0900

    演習課題１

--- pygame_image.py
+++ pygame_image.py
@@ -20,7 +20,7 @@ def main():
         
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_img_lst[tmr%2], [300, 200])
+        screen.blit(kk_img_lst[tmr%100//50], [300, 200])
         pg.display.update()
         tmr += 1 
         x += 1       

78982e620a247f8760b2274dc487a4b9dd6ed23c
ryuseiy <c0b2215016@edu.teu.ac.jp>
2023-06-13 15:47:58 +0900

    練習５

--- pygame_image.py
+++ pygame_image.py
@@ -4,17 +4,20 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img_o = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_img_lst = [kk_img, kk_img_o]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img_lst[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
